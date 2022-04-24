9c28e03cc509624908ca90d96d8e547bb6529423
Rui2020pro <matosrui1998@gmail.com>
2022-04-24 19:44:43 +0100

    customize email and build email to user

--- app_gestao_mensagens_notificacoes/resources/views/vendor/mail/html/header.blade.php
+++ app_gestao_mensagens_notificacoes/resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,8 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+@if (trim($slot) === 'Gestão de Mensagens e Notificações')
+<img src="{{ asset('img/logo.png') }}" class="logo" alt="Gestão de Mensagens e Notificações Logo">
 @else
 {{ $slot }}
 @endif
